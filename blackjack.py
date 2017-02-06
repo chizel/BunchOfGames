2fec516518e76d0e42dc1847a87ad5111534ca82
Denis <prohibiti@gmail.com>
2017-02-06 15:46:23 +0200

    blackjack

---
+++ blackjack.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Deck():
+    def __init__(self, ):
+        if position < 0:
+            self.generate_position()
+        else:
+            self.position = position % 4
+        self.on = False
+
+    def generate(self):
+        card = []
+        rank = [range(2, 11)] + ['J', 'Q', 'K', 'A']
+        pass
+
+    def get_card(self):
+        pass
+
+    def value(self, hand):
+        # aces to the end
+        for card in hand:
+            res += hand.value
+
+class BlackJack():
+    def __init__(self):
+        pass
+
+
+def main():
+    mg = Pipes(5, 5)
+    return
+
+
+if __name__ == "__main__":
+    main()
